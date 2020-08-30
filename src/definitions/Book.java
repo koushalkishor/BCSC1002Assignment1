5d6679e18ddded7316fa59265f1563a31f9cef6b
koushalkishor <koushal.kishor_cs19@gla.ac.in>
2020-08-30 18:51:52 +0530

    create getter and setter methods for fields of Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,7 +1,7 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  User: Koushal kishor
+ *  Date: 30/08/20
+ *  Time: 6:28 PM
  *  File Name : Book.java
  * */
 package definitions;
@@ -9,4 +9,30 @@ package definitions;
 public class Book {
     private String bookName;
     private String authorName;
+    private String ISBNNumber;
+
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getISBNNumber() {
+        return ISBNNumber;
+    }
+
+    public void setISBNNumber(String ISBNNumber) {
+        this.ISBNNumber = ISBNNumber;
+    }
 }
