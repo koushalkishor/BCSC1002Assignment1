d29e8213261e19a816e33bc5efc4fe81f9ea4409
koushalkishor <koushal.kishor_cs19@gla.ac.in>
2020-08-30 20:18:49 +0530

    create getter and setter methods for each private fields

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -13,4 +13,54 @@ public class Student {
     private long universityRollNumber;
     private int numberOfBooksIssued;
     private Book[] namesOfTheBooksIssued;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public long getUniversityRollNumber() {
+        return universityRollNumber;
+    }
+
+    public void setUniversityRollNumber(long universityRollNumber) {
+        this.universityRollNumber = universityRollNumber;
+    }
+
+
+    public int getNumberOfBooksIssued() {
+        return numberOfBooksIssued;
+    }
+
+    public void setNumberOfBooksIssued(int numberOfBooksIssued) {
+        this.numberOfBooksIssued = numberOfBooksIssued;
+    }
+
+
+    public Book[] getNamesOfTheBooksIssued() {
+        return namesOfTheBooksIssued;
+    }
 }
